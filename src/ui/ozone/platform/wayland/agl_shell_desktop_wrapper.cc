9d74afbbd57044e8bf89263b22e544d03e6768e9
Jose Dapena Paz <jdapena@igalia.com>
2021-11-04 11:55:07 +0100

    Drop libhomescreen dependency.
    The application framework has been dropped, homescreen library does
    not exist anymore.
    
    [SPEC-4121] Prepare master branch for new app fw integration
    https://jira.automotivelinux.org/browse/SPEC-4121

--- src/ui/ozone/platform/wayland/agl_shell_desktop_wrapper.cc
+++ src/ui/ozone/platform/wayland/agl_shell_desktop_wrapper.cc
@@ -8,31 +8,10 @@
 
 namespace ui {
 
-AglShellDesktop::AglShellDesktop(WaylandConnection *wconnection, std::string app_id, int port)
+AglShellDesktop::AglShellDesktop(WaylandConnection *wconnection, std::string app_id)
   : connection_(wconnection)
   , agl_shell_desktop_(wconnection->ashell_desktop())
-  , app_id_(app_id)
-{
-    homescreen_.init(port, "wm");
-
-    homescreen_.set_event_handler(LibHomeScreen::Event_ShowWindow, [this] (json_object* object) {
-      struct json_object *obj_app_id;
-      if (json_object_object_get_ex(object, "application_id", &obj_app_id)) {
-        std::string app_id = json_object_get_string(obj_app_id);
-        LOG(INFO) << "showWindow received for " << app_id;
-        if (app_id == app_id_) {
-            LOG(INFO) << "Activate app " << app_id_;
-
-            WaylandOutputManager *wayland_manager = connection_->wayland_output_manager();
-            WaylandOutput *wayland_output = wayland_manager->GetPrimaryOutput();
-            agl_shell_desktop_activate_app(agl_shell_desktop_, app_id_.c_str(), nullptr, wayland_output->output());
-            connection_->ScheduleFlush();
-        }
-      } else {
-        LOG(ERROR) << "Failed to parse json object";
-      }
-    });
-}
+  , app_id_(app_id) {}
 
 AglShellDesktop::~AglShellDesktop()
 {
